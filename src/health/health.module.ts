446b434faad3b60fb1c5a961af52420826e7a6c4
Wooong <osw@daum.net>
2023-12-04 22:11:46 +0900

    chore: 참고 내용 주석으로 추가

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
+
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [TerminusModule, HttpModule],
+  imports: [TerminusModule, HttpModule], // gracefulShutdownTimeoutMs: 1000,
   controllers: [HealthController],
 })
 export class HealthModule {}
